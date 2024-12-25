5c579c17e308330e1e44eefd16afc13ee8ae67c0
asema <shaiycbekovaasemaasema@gmail.com>
2024-12-25 15:17:52 +0600

    jaefjehfj

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -150,6 +150,7 @@ MODELTRANSLATION_LANGUAGES = ('en', 'ru')
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
